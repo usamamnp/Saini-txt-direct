4621c1e73ec5483e0f51c654ace49dac926e8100
usamamnp <Iusamasohail1@gmail.com>
2025-06-30 09:51:17 +0500

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "xxxxxxxx"))
 API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxx")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8186384499:AAEq1UAEqA9JLEstowuAVKrw4GJj0hofjCI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "5680454765"))
 CREDIT = "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
 LOG_CHANNEL = "-1002755894888" # log group me bot ko admin narur banaye full rights ke sath
